70a7af419a6982fcb103075555a04d3fdfee0886
David Hew-wing <dhew67@gmail.com>
2021-01-04 00:51:34 -0500

    updated for cloud scheduler and cloud function support

--- main.py
+++ main.py
@@ -22,10 +22,14 @@ print('Authentication Complete! \U0001F680 \n')
 master_controller.run(settings.secret_map['account'])
 
 def run(request):
-    request_json = request.get_json()
-    if request.args and 'message' in request.args:
-        return request.args.get('message')
-    elif request_json and 'message' in request_json:
-        return request_json['message']
-    else:
-        return f'Finished'
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "./src/tokens/credentials.json"
+    # # Authentication
+    settings.get_secrets()
+    account = datastore_helper.retrieve_account_by_api_key(settings.secret_map)
+    settings.add_to_secrets('account', account)
+    qtauth.qt_authentication(settings.secret_map)
+    gsauth.gs_auth()
+    print('Authentication Complete! \U0001F680 \n')
+
+    master_controller.run(settings.secret_map['account'])
+    return f'Finished'
